ee35186d262dc6132a572ddb56ee3bd1a9d2c57c
m.chuduk <m.chuduk@freshlimesoft.com>
2022-04-19 10:45:04 +0300

    add config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+const config = require('./config');
 const express = require('express');
 
 function bootstrap() {
   const app = express();
 
-  const port = process.env.PORT || 3000;
+  const port = config.PORT || 3000;
   app.listen(port, () => console.log(`server was started:${port}`));
 }
 
